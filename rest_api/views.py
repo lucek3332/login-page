27d8c2bc431b2a63ed1184bd7eb56eee75f93d2b
lucek3332 <jakubszulgit@gmail.com>
2020-12-22 15:53:01 +0100

    Feat [rest_api]: Add UserSerializer and UserView
    + create serializers.py and add UserSerializer using built-in User model
    + create UserView, which inherits from ListAPIView for testing purpose
    + create rest_api.urls

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class UserView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
