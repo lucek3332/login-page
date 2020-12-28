88d01a76b4b59b15768daa354a573afd23d805f2
lucek3332 <jakubszulgit@gmail.com>
2020-12-28 18:13:36 +0100

    Feat [frontend]: Modify LoginPage component
    + add new state parameters: success_msg and error
    + add getLoginStatus method, which fetch to API endpoint and get back information about authentication
    ~ modify form submit, change button type to submit, add preventDefault() function to handleLoginButtonPressed
    ~ update state in handleLoginButtonPressed method after getting the response
    + add welcome info, if user is authenticated
    + display error and success messages

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import { Link, Redirect } from "react-router-dom";
-import CsrfToken from "./csrfToken";
+import CsrfToken from "./CsrfToken";
 
 
 export default class LoginPage extends Component {
@@ -9,14 +9,43 @@ export default class LoginPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: "",
-            password: "",
-            isLoggedIn: false
+            error: "",
+            success_msg: ""
         };
 
         this.handleUsernameChange = this.handleUsernameChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.handleLoginButtonPressed = this.handleLoginButtonPressed.bind(this);
+        this.getLoginStatus = this.getLoginStatus.bind(this);
+
+        this.getLoginStatus();
+
+    }
+
+    getLoginStatus() {
+        const requestOptions = {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        };
+
+        fetch("/api/login-status/", requestOptions)
+            .then((response) => {
+                if (response.ok) {
+                    this.setState({
+                        isLoggedIn: true,
+                        });
+                    return response.json();
+                } else {
+                    this.setState({
+                        isLoggedIn: false,
+                    });
+                }
+            })
+            .then((data) => {
+                this.setState({
+                    username: data.username,
+                });
+            });
 
     }
 
@@ -32,7 +61,10 @@ export default class LoginPage extends Component {
         });
     }
 
-    handleLoginButtonPressed() {
+    handleLoginButtonPressed(e) {
+
+        e.preventDefault();
+
         const requestOptions = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -43,68 +75,113 @@ export default class LoginPage extends Component {
         };
 
         fetch("/api/login/", requestOptions)
-          .then((response) => response.json)
-          .then((data) => console.log(data));
+          .then((response) => {
+            if (response.ok) {
+                this.setState({
+                    password: "",
+                    isLoggedIn: true,
+                    success_msg: "Logowanie pomyślne",
+                    });
+            } else {
+                this.setState({
+                    password: "",
+                    error: "Logowanie nieudane",
+                });
+            }
+          });
 
     }
 
     render() {
-        return (
-            <div class="modal-dialog text-center">
-                <div class="col-sm-9 mx-auto main-section">
-                    <div class="modal-content">
-                        <div class="col-12 user-img">
-                            <img src="../static/img/user.png" />
-                        </div>
+        const isLoggedIn = this.state.isLoggedIn;
+        if (!isLoggedIn) {
+            return (
+                <div class="modal-dialog text-center">
+                    <div class="col-sm-9 mx-auto main-section">
+                        <div class="modal-content">
+                            <div class="col-12 user-img">
+                                <img src="../static/img/user.png" />
+                            </div>
 
-                        <div class="col-10 mx-auto form-input">
-                            <form action="." method="post">
+                            <div class="col-10 mx-auto form-input">
+                                <form action="." method="post" onSubmit={this.handleLoginButtonPressed}>
 
-                                <CsrfToken />
+                                    <CsrfToken />
 
-                                <div class="form-group mb-3">
-                                    <i class="fas fa-user"></i>
-                                    <input
-                                        type="text"
-                                        class="form-control"
-                                        id="id_username"
-                                        name="username"
-                                        required=""
-                                        placeholder="Login"
-                                        autocomplete="off"
-                                        autofocus=""
-                                        autocapitalize="none"
-                                        maxlength="150"
-                                        onChange={this.handleUsernameChange}
-                                    />
-                                </div>
+                                    {this.state.error ? (
+                                        <div class="alert alert-warning p-1" role="alert">
+                                            {this.state.error}
+                                        </div>
+                                    ) : (
+                                        <div></div>
+                                    )}
 
-                                <div class="form-group mb-3">
-                                    <i class="fas fa-key"></i>
-                                    <input
-                                        type="password"
-                                        class="form-control"
-                                        id="id_password"
-                                        name="password"
-                                        required=""
-                                        placeholder="Password"
-                                        autocomplete="current-password"
-                                        onChange={this.handlePasswordChange}
-                                    />
-                                </div>
+                                    <div class="form-group mb-3">
+                                        <i class="fas fa-user"></i>
+                                        <input
+                                            type="text"
+                                            class="form-control"
+                                            id="id_username"
+                                            name="username"
+                                            required=""
+                                            placeholder="Login"
+                                            autocomplete="off"
+                                            autofocus=""
+                                            autocapitalize="none"
+                                            maxlength="150"
+                                            onChange={this.handleUsernameChange}
+                                        />
+                                    </div>
 
-                                <button
-                                    type="button"
-                                    class="btn btn-success mb-4"
-                                    onClick={this.handleLoginButtonPressed}
-                                >
-                                    Zaloguj
-                                </button>
-                            </form>
+                                    <div class="form-group mb-3">
+                                        <i class="fas fa-key"></i>
+                                        <input
+                                            type="password"
+                                            class="form-control"
+                                            id="id_password"
+                                            name="password"
+                                            required=""
+                                            placeholder="Password"
+                                            autocomplete="current-password"
+                                            onChange={this.handlePasswordChange}
+                                        />
+                                    </div>
+
+                                    <button
+                                        type="submit"
+                                        class="btn btn-success mb-4"
+                                    >
+                                        Zaloguj
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div class="modal-dialog text-center">
+                    <div class="col-sm-9 mx-auto main-section">
+                        <div class="modal-content">
+                            <div class="col-8 mx-auto">
+                                {this.state.success_msg ? (
+                                    <div class="alert alert-success p-1 mt-4 mb-1" role="alert">
+                                        {this.state.success_msg}
+                                    </div>
+                                ) : (
+                                    <div></div>
+                                )}
+                            </div>
+
+                            <div class="col-10 mx-auto user-login">
+                                <h1>Cześć {this.username}!</h1>
+                                <button class="btn btn-success mb-4">Wyloguj</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
     }
 }
