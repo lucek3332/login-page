d077a95f3f43ca9c3749f63b6d5aa8bbef25673a
lucek3332 <jakubszulgit@gmail.com>
2020-12-30 14:37:22 +0100

    Refactor [pure_django]: Refactor code for readability
    ~ unification quotation marks and apostrophes
    ~ modify of import order
    ~ refactor HTML tags in login.html

--- pure_django/views.py
+++ pure_django/views.py
@@ -1,11 +1,11 @@
-from .forms import LoginForm
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from .forms import LoginForm
 
 
 class CustomLoginView(SuccessMessageMixin, LoginView):
-    template_name = 'pure_django/accounts/login.html'
+    template_name = "pure_django/accounts/login.html"
     form_class = LoginForm
     success_message = "Logowanie pomyślne"
 
