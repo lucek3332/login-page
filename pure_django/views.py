293c129d4562417ae18ac78fe94edffab58990b5
lucek3332 <jakubszulgit@gmail.com>
2021-01-16 15:04:20 +0100

    Feat [pure_django]: Create custom login and logout views
    + create login view with success and error messages
    + create simple logout view

--- pure_django/views.py
+++ pure_django/views.py
@@ -1,7 +1,10 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from .forms import LoginForm
+from .forms import CustomLoginForm, LoginForm
 
 
 class CustomLoginView(SuccessMessageMixin, LoginView):
@@ -11,3 +14,33 @@ class CustomLoginView(SuccessMessageMixin, LoginView):
 
     def get_success_url(self):
         return reverse_lazy("pure_django:login")
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = CustomLoginForm(data=request.POST)
+
+        if form.is_valid():
+            clean_data = form.cleaned_data
+            username = clean_data.get("username")
+            password = clean_data.get("password")
+
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+                messages.success(request, "Logowanie pomyślne")
+
+                return redirect("pure_django:login")
+
+            messages.error(request, "Logowanie nieudane")
+
+    else:
+        form = CustomLoginForm()
+
+    return render(request, "pure_django/accounts/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("pure_django:login")
